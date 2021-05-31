4dfc8e8c487865416f5b1fce9380134313c40c53
akeenahall42029 <akeenadhall@gmail.com>
2021-05-31 12:48:16 -0400

    created the website with basic functionality

---
+++ designs.py
@@ -0,0 +1,12 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+#colors for the turtle 
+dark green = (2,153,0)
+light_green = (149,185,11)
+black = (0,0,0)
+sandy = (252,242,186)
+
+turtle = []
